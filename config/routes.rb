7464a169551b9b0478ef901a3cdd03dadec1fc60
Saud Saleem <saudsaleem.dev@gmail.com>
2023-04-12 06:21:24 -0700

    Add routes and teams CRUD API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :members
+  resources :teams
+  resources :projects
+
+  get '/teams/:id/members', to: 'teams#members', as: 'team_members'
+  get '/projects/:project_id/members', to: 'projects#members', as: 'project_members'
+  patch '/projects/:project_id/members/:member_id', to: 'projects#add_member', as: 'add_member_to_project'
 end
